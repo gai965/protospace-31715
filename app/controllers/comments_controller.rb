2a94f1d8410dbc74f35ffde0f74be814d81d5231
gai965 <kotodama51080@gmail.com>
2020-10-31 22:26:39 +0900

    「空のコメントでは保存されない」を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    if comment.save
-      redirect_to prototype_path(comment.prototype.id)
+   @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype_id)
     else
-      render :show
+      @prototype = Prototype.find(@comment.prototype_id)
+      @comments = Comment.where(prototype_id: @prototype.id)
+      render "prototypes/show"
     end
   end
 
